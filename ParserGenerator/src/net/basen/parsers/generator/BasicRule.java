b2c6fdff4ba094706fe4f93c5dad6cd0f61a187d
Luis Colorado <lcu@basen.net>
2016-08-25 14:03:56 +0300

    Initial revision
    Signed-off-by: Luis Colorado <lcu@basen.net>

---
+++ ParserGenerator/src/net/basen/parsers/generator/BasicRule.java
@@ -0,0 +1,78 @@
+/* $Id$
+ * Author: Luis Colorado <lcu@basen.net>
+ * Date: Aug 25, 2016 11:52:40 AM.
+ * Project: ParserGenerator
+ * Package: net.basen.parsers.generator
+ * Disclaimer: (C) 2016 LUIS COLORADO.  All rights reserved.
+ */
+
+package net.basen.parsers.generator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Luis Colorado {@code <lcu@basen.net>}.
+ *
+ */
+public class BasicRule<S extends Symbol<S>> 
+	extends ArrayList<S>
+{
+	private static long next_id = 0;
+	private static final long serialVersionUID = 6212726366672386484L;
+	private long m_id;
+	private S m_lhs;
+	
+	public BasicRule() {
+		m_id = next_id++;
+	}
+	
+	@SafeVarargs
+	public BasicRule(S lhs, S... rhs) {
+		super(rhs.length);
+		m_id = next_id++;
+		m_lhs = lhs;
+		for(S s: rhs) add(s);
+	}
+
+	public BasicRule(S lhs, List<S> rhs) {
+		m_id = next_id++;
+		m_lhs = lhs;
+		addAll(rhs);
+	}
+
+	/**
+	 * Getter for the {@code long} {@code id} attribute.
+	 * @return the {@code long} {@code id}.
+	 */
+	public long getId() {
+		return m_id;
+	}
+
+	/**
+	 * Getter for the {@code S} {@code lhs} attribute.
+	 * @return the {@code S} {@code lhs}.
+	 */
+	public S getLhs() {
+		return m_lhs;
+	}
+
+	/**
+	 * Setter for the {@code S} {@code lhs.
+	 * @param lhs the {@code S} {@code lhs} to set.
+	 */
+	public void setLhs(S lhs) {
+		m_lhs = lhs;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(m_lhs + ":");  // can raise NullPointerException.
+		if (isEmpty())
+			sb.append(" /* empty */");
+		else for (S s: this)
+			sb.append(" " + s);
+		sb.append(";");
+		return sb.toString();
+	}
+}
---
+++ ParserGenerator/test/net/basen/parsers/generator/TestRule.java
@@ -0,0 +1,86 @@
+/* $Id$
+ * Author: Luis Colorado <lcu@basen.net>
+ * Date: Aug 25, 2016 12:58:49 PM.
+ * Project: ParserGenerator
+ * Package: net.basen.parsers.generator
+ * Disclaimer: (C) 2016 LUIS COLORADO.  All rights reserved.
+ */
+
+package net.basen.parsers.generator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * @author  {@code <>}.
+ *
+ */
+public class TestRule {
+	
+	static enum Symbols implements Symbol<Symbols> {
+		expr, term, factor, NEGATION, AND, OR, LEFT_PAREN, RIGHT_PAREN, IDENTIFIER,
+	}
+	
+	BasicRule<Symbols> iut;
+	
+	@Test
+	public void testEmptyConstructor() {
+		iut = new BasicRule<Symbols>();
+		assertNotNull(iut);
+		iut.setLhs(Symbols.expr);
+		assertEquals("expr: /* empty */;", iut.toString());
+	}
+	
+	@Test
+	public void testNoLhs() {
+		iut = new BasicRule<Symbols>();
+		assertNotNull(iut);
+		assertEquals("null: /* empty */;", iut.toString());
+	}
+	
+	@Test
+	public void testPopulate1() {
+		iut = new BasicRule<Symbols>();
+		iut.setLhs(Symbols.expr);
+		iut.add(Symbols.expr);
+		iut.add(Symbols.OR);
+		iut.add(Symbols.term);
+		assertEquals("expr: expr OR term;", iut.toString());
+	}
+	
+	@Test
+	public void testPopulate2() {
+		iut = new BasicRule<Symbols>(Symbols.term, Symbols.term, Symbols.AND, Symbols.factor);
+		assertEquals("term: term AND factor;", iut.toString());
+	}
+	
+	@Test
+	public void testPopulate3() {
+		iut = new BasicRule<Symbols>(Symbols.factor, new ArrayList<Symbols>(
+				Arrays.asList(Symbols.LEFT_PAREN, Symbols.expr, Symbols.RIGHT_PAREN)));
+		assertEquals("factor: LEFT_PAREN expr RIGHT_PAREN;", iut.toString());
+	}
+	
+	@Test
+	public void testCreateSeveralRules() {
+		BasicRule<Symbols> R1 = new BasicRule<Symbols>(Symbols.expr, Symbols.expr, Symbols.OR, Symbols.term);
+		BasicRule<Symbols> R2 = new BasicRule<Symbols>(Symbols.expr, Symbols.term);
+		BasicRule<Symbols> R3 = new BasicRule<Symbols>(Symbols.term, Symbols.term, Symbols.AND, Symbols.factor);
+		BasicRule<Symbols> R4 = new BasicRule<Symbols>(Symbols.term, Symbols.factor);
+		BasicRule<Symbols> R5 = new BasicRule<Symbols>(Symbols.factor, Symbols.NEGATION, Symbols.factor);
+		BasicRule<Symbols> R6 = new BasicRule<Symbols>(Symbols.factor, Symbols.LEFT_PAREN, Symbols.expr, Symbols.RIGHT_PAREN);
+		BasicRule<Symbols> R7 = new BasicRule<Symbols>(Symbols.factor, Symbols.IDENTIFIER);
+		assertEquals("expr: expr OR term;", R1.toString());
+		assertEquals("expr: term;", R2.toString());
+		assertEquals("term: term AND factor;", R3.toString());
+		assertEquals("term: factor;", R4.toString());
+		assertEquals("factor: NEGATION factor;", R5.toString());
+		assertEquals("factor: LEFT_PAREN expr RIGHT_PAREN;", R6.toString());
+		assertEquals("factor: IDENTIFIER;", R7.toString());
+	}
+}
